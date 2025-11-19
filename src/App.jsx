85c9341891553bdb9e4e48b90e3328e0510ffcba
programadorFB <devfuzabalta@gmail.com>
2025-11-19 18:13:07 -0300

    novas feat

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ const ROULETTE_GAME_IDS = {
 };
 
 const filterOptions = [
-  { value: 50, label: 'Últimas 50 Rodadas' },
+  // { value: 50, label: 'Últimas 50 Rodadas' },
   { value: 100, label: 'Últimas 100 Rodadas' },
   { value: 300, label: 'Últimas 300 Rodadas' },
   { value: 500, label: 'Últimas 500 Rodadas' },
@@ -360,7 +360,7 @@ const App = () => {
   const [jwtToken, setJwtToken] = useState(null);
   const [isPaywallOpen, setIsPaywallOpen] = useState(false);
   const [checkoutUrl, setCheckoutUrl] = useState('');
-  
+  const isPremium = false;
   // App States
   const [selectedRoulette, setSelectedRoulette] = useState(Object.keys(ROULETTE_SOURCES)[0]);
   const [spinHistory, setSpinHistory] = useState([]);
@@ -896,9 +896,19 @@ const App = () => {
                     className="roulette-selector" 
                     value={selectedRoulette}
                     onChange={(e) => {
+                      // DADOS ATUAIS (COM O BUG)
+                      // setSelectedRoulette(e.target.value);
+                      // setLaunchError('');
+                      // setGameUrl(''); 
+                      
+                      // --- CORREÇÃO ---
+                      // Limpe o histórico e o resultado selecionado ao trocar
+                      setSpinHistory([]);
+                      setSelectedResult(null); 
+                      // O resto do seu código
                       setSelectedRoulette(e.target.value);
                       setLaunchError('');
-                      setGameUrl(''); // Adicionado: Força o auto-launch a rodar de novo se mudar a roleta
+                      setGameUrl(''); 
                     }}
                   >
                     {Object.keys(ROULETTE_SOURCES).map(key => (
@@ -1022,10 +1032,12 @@ const App = () => {
                 <div className='latest-results'>
 
                   <ResultsGrid 
-                    latestNumbers={stats.latestNumbers}
-                    numberPullStats={numberPullStats}
-                    numberPreviousStats={numberPreviousStats}
-                    onResultClick={handleResultBoxClick}
+                  latestNumbers={stats.latestNumbers}
+                  numberPullStats={numberPullStats}
+                  numberPreviousStats={numberPreviousStats}
+                  onResultClick={handleResultBoxClick}
+                  isPremium={isPremium} // Variável que diz se o user é premium
+                  setIsPaywallOpen={setIsPaywallOpen} // Função que abre o modal
                     />
                 </div>
                     </div>
