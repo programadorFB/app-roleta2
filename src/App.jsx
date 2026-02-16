143744b829d7b4a3b717d7ff948a3ee97f88b537
programadorFB <devfuzabalta@gmail.com>
2026-02-16 16:28:17 -0300

    tchau playtech

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const getNumberColor = (num) => {
 };
 
 const ROULETTE_SOURCES = {
-  brasilPlay: '🇧🇷  Brasileira - Playtech',
+  // brasilPlay: '🇧🇷  Brasileira - Playtech',
   aovivo: '🇧🇷 Roleta Brasileira  - Evolution',
   immersive: '🌟 Immersive Roulette - Evolution',
   viproulette: '💎 Vip Roulette - Evolution',
