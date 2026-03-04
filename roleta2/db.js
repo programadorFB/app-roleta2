e52c4a5cbf359afcb75e06b78a7c46b71e89cfae
programadorFB <devfuzabalta@gmail.com>
2026-03-04 14:07:21 -0300

    db apontado pro lado correto

--- roleta2/db.js
+++ roleta2/db.js
@@ -12,7 +12,7 @@ const { Pool } = pg;
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'fuzabalta_roulette',
+  database: process.env.DB_NAME || 'phantom-roleta',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || '1234',
   max: 20, // Máximo de conexões no pool
